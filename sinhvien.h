623fe243a344fe10a4001cff234ee38923614dc3
ducozil <93061693+ducozil@users.noreply.github.com>
2023-03-20 12:12:10 +0700

    Add files via upload

---
+++ sinhvien.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class SinhVien{
+    private:
+         string MaSV;
+         string TenSV;
+    public:
+         SinhVien();
+         SinhVien(stringMaSV);
+         SinhVien(string MaSV, string TenSV);
+         void
+
+}
